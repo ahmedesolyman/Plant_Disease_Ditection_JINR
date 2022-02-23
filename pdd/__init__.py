f43ee5d4b80b06ca812b9c97e0413d7a48abea15
ahmedesolyman <ahmedesolyman@gmail.com>
2022-02-23 16:14:13 +0200

    modification with ALEX

--- pdd/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-from . import models
-from . import utils
-from . import losses
-from . import distances
-
-__version__ = '1.0.0'
--- pdd/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-
-from .knn import TfKNN
-from .siamese import make_siamese
-from .feature_extractor import get_feature_extractor
